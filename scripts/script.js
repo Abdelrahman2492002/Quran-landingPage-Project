24976ef1db45d6afb352b26940231086d205c56e
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2024-11-07 13:31:03 +0200

    Remove unnecessary files and keep dist only

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-import { labelClickEvent, oberverElement } from "./util.js";
-
-const labelElements = document.querySelectorAll("label");
-const sectionElements = document.querySelectorAll("section");
-
-labelElements.forEach(labelClickEvent);
-
-oberverElement(sectionElements);
